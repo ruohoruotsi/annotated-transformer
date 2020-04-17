51906fbd60a8bb94a8546017f6749f3ba6f31630
Iroro Orife <iorife@netflix.com>
2020-04-17 15:20:32 -0700

    [ADD] more

--- annotated_transformer.py
+++ annotated_transformer.py
@@ -271,8 +271,35 @@ def make_model(src_vocab, tgt_vocab, N=6,
     return model
 
 #####################################################################################################################
-## Training Loop
+# Training
 #####################################################################################################################
+
+
+# Batches and Masking
+
+
+class Batch:
+    """Object for holding a batch of data with mask during training."""
+
+    def __init__(self, src, trg=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if trg is not None:
+            self.trg = trg[:, :-1]
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = \
+                self.make_std_mask(self.trg, pad)
+            self.ntokens = (self.trg_y != pad).data.sum()
+
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        """Create a mask to hide padding and future words."""
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & Variable(
+            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+        return tgt_mask
+
+# Training Loop
 
 
 def run_epoch(data_iter, model, loss_compute):
@@ -312,6 +339,8 @@ def batch_size_fn(new, count, sofar):
     tgt_elements = count * max_tgt_in_batch
     return max(src_elements, tgt_elements)
 
+
+# Optimizer
 
 class NoamOpt:
     """Optim wrapper that implements rate."""
@@ -353,3 +382,116 @@ opts = [NoamOpt(512, 1, 4000, None),
         NoamOpt(256, 1, 4000, None)]
 
 #####################################################################################################################
+# Regularization
+#####################################################################################################################
+
+# We implement label smoothing using the KL div loss. Instead of using a one-hot target distribution, we create a
+# distribution that has confidence of the correct word and the rest of the smoothing mass distributed throughout
+# the vocabulary
+
+
+class LabelSmoothing(nn.Module):
+    """Implement label smoothing."""
+
+    def __init__(self, size, padding_idx, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = nn.KLDivLoss(size_average=False)
+        self.padding_idx = padding_idx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    def forward(self, x, target):
+        assert x.size(1) == self.size
+        true_dist = x.data.clone()
+        true_dist.fill_(self.smoothing / (self.size - 2))
+        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        true_dist[:, self.padding_idx] = 0
+        mask = torch.nonzero(target.data == self.padding_idx)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        return self.criterion(x, Variable(true_dist, requires_grad=False))
+
+#####################################################################################################################
+# Synthetic Data
+
+
+def data_gen(V, batch, nbatches):
+    """Generate random data for a src-tgt copy task."""
+    for i in range(nbatches):
+        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
+        data[:, 0] = 1
+        src = Variable(data, requires_grad=False)
+        tgt = Variable(data, requires_grad=False)
+        yield Batch(src, tgt, 0)
+
+#####################################################################################################################
+# Loss Computation
+
+
+class SimpleLossCompute:
+    """A simple loss compute and train function."""
+
+    def __init__(self, generator, criterion, opt=None):
+        self.generator = generator
+        self.criterion = criterion
+        self.opt = opt
+
+    def __call__(self, x, y, norm):
+        x = self.generator(x)
+        loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
+                              y.contiguous().view(-1)) / norm
+        loss.backward()
+        if self.opt is not None:
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+        return loss.data[0] * norm
+
+
+
+#####################################################################################################################
+# Greedy Decoding - This code predicts a translation using greedy decoding for simplicity.
+
+
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+    for i in range(max_len - 1):
+        out = model.decode(memory, src_mask,
+                           Variable(ys),
+                           Variable(subsequent_mask(ys.size(1))
+                                    .type_as(src.data)))
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat([ys,
+                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+    return ys
+
+
+#####################################################################################################################
+# Train the simple copy task.
+
+
+V = 11
+
+criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+model = make_model(V, V, N=2)
+model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
+                    torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+for epoch in range(10):
+    model.train()
+    run_epoch(data_gen(V, 30, 20), model,
+              SimpleLossCompute(model.generator, criterion, model_opt))
+    model.eval()
+    print(run_epoch(data_gen(V, 30, 5), model,
+                    SimpleLossCompute(model.generator, criterion, None)))
+
+
+model.eval()
+src = Variable(torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]))
+src_mask = Variable(torch.ones(1, 1, 10))
+print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
